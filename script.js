b9f50d9b19ccd6d21ea6a2ade986a41590e5adfa
horus911 <34630037+horus911@users.noreply.github.com>
2025-05-28 11:49:03 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function updateShareLink() {
-  const data = {
-    text: textInput.value,
-    files: filesData
-  };
-  const encoded = encodeURIComponent(JSON.stringify(data));
-  const url = `${location.origin}${location.pathname}?data=${encoded}`;
-  shareLinkInput.value = url;
-}
